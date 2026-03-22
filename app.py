47895c2ed4575560096ba1b8edd3daf28c22e341
kyle01041981 <kyle01041981@gmail.com>
2026-03-22 17:56:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ Link: {data.get('url')}
 """
 
     send_email(subject, body)
-    return {"status": "ok"}
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
